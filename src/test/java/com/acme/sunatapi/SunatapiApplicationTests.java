d702b9c65b97329eb2dff0b2ff1ea2fea05ab415
diegocho <diegoalonzo2001@hotmail.com>
2022-09-18 20:50:43 -0500

    creacion

---
+++ src/test/java/com/acme/sunatapi/SunatapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.sunatapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SunatapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
